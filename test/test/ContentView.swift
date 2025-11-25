87a569f57e01b22346de34ad1e627cecf66c4234
SaiEaranti <saipreetam.earanti@icloud.com>
2025-11-25 16:58:13 -0600

    new

--- test/test/ContentView.swift
+++ test/test/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world! hello james")
+            Text("hi vaishvik ur weird")
         }
         .padding()
     }
