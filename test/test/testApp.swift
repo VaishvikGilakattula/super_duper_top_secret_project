874a9ebfda305da8977d9e487af6bc406e0c8891
SaiEaranti <saipreetam.earanti@icloud.com>
2025-11-25 16:04:19 -0600

    Initial commit

---
+++ test/test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Saipreetam Earanti on 11/25/25.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
